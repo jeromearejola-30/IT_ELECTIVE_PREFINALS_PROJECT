3637fb0310538dd0c4a6dcf129aa26468f033809
jeromearejola-30 <jeromearejola30@gmail.com>
2026-08-21 22:49:11 +0800

    add nav links for Departments and Employees in shared layout

---
+++ PREFINALS_PROJECT/PREFINALS_PROJECT/Controllers/EmployeesController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using PREFINALS_PROJECT.Data;
+
+namespace PREFINALS_PROJECT.Controllers
+{
+    public class EmployeesController : Controller
+    {
+        private readonly AppDbContext _context;
+
+        public EmployeesController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var employees = await _context.Employees
+                .Include(e => e.Department)
+                .ToListAsync();
+
+            return View(employees);
+        }
+    }
+}
