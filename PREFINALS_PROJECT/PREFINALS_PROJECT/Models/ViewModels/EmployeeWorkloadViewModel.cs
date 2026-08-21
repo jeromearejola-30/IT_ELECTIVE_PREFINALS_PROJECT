0a83f1e31224261bc0f254b6dacdc91c89e45e62
jeromearejola-30 <jeromearejola30@gmail.com>
2026-08-21 21:09:52 +0800

    finished setting up the files

---
+++ PREFINALS_PROJECT/PREFINALS_PROJECT/Models/ViewModels/EmployeeWorkloadViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace PREFINALS_PROJECT.Models.ViewModels
+{
+    public class EmployeeWorkloadViewModel
+    {
+        public string EmployeeName { get; set; } = string.Empty;
+        public string DepartmentName { get; set; } = string.Empty;
+        public int UnresolvedTicketCount { get; set; }
+    }
+}
