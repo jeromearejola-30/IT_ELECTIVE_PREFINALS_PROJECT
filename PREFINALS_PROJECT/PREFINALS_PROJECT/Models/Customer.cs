59158e6ab8553cab03e9ed03f87b66018ef291db
jpacaonces <janssen.m.pacaonces@gmail.com>
2026-08-21 23:36:58 +0800

    create AppDbContext mapping tables and self-referencing relationship

--- PREFINALS_PROJECT/PREFINALS_PROJECT/Models/Customer.cs
+++ PREFINALS_PROJECT/PREFINALS_PROJECT/Models/Customer.cs
@@ -1,6 +1,28 @@
-﻿namespace PREFINALS_PROJECT.Models
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace PREFINALS_PROJECT.Models
 {
+    [Table("Customers")]
     public class Customer
     {
+        [Key]
+        public int CustomerId { get; set; }
+
+        [Required]
+        [StringLength(100)]
+        public string CompanyName { get; set; } = string.Empty;
+
+        [Required]
+        [StringLength(100)]
+        public string ContactName { get; set; } = string.Empty;
+
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; } = string.Empty;
+
+        public string? Phone { get; set; }
+
+        public bool IsActive { get; set; }
     }
 }
--- PREFINALS_PROJECT/PREFINALS_PROJECT/Models/Team.cs
+++ PREFINALS_PROJECT/PREFINALS_PROJECT/Models/Team.cs
@@ -1,6 +1,22 @@
-﻿namespace PREFINALS_PROJECT.Models
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace PREFINALS_PROJECT.Models
 {
+    [Table("Teams")]
     public class Team
     {
+        [Key]
+        public int TeamId { get; set; }
+
+        [Required]
+        [StringLength(100)]
+        public string Name { get; set; } = string.Empty;
+
+        [Required]
+        public int DepartmentId { get; set; }
+
+        [ForeignKey(nameof(DepartmentId))]
+        public Department Department { get; set; } = null!;
     }
 }
