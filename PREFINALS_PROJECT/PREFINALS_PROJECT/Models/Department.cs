40bf0f89941bd67bca133f0364b4f63daf505f47
jeromearejola-30 <jeromearejola30@gmail.com>
2026-08-21 22:34:26 +0800

    add Department and Employee models with Data Annotations

---
+++ PREFINALS_PROJECT/PREFINALS_PROJECT/Models/Department.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace PREFINALS_PROJECT.Models
+{
+    [Table("Departments")]
+    public class Department
+    {
+        [Key]
+        public int DepartmentId { get; set; }
+
+        [Required]
+        [StringLength(100)]
+        public string Name { get; set; } = string.Empty;
+
+        public string? Description { get; set; }
+
+        public bool IsActive { get; set; }
+
+        // Navigation Property
+        public ICollection<Employee> Employees { get; set; } = new List<Employee>();
+    }
+}
---
+++ PREFINALS_PROJECT/PREFINALS_PROJECT/Models/Employee.cs
@@ -0,0 +1,38 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace PREFINALS_PROJECT.Models
+{
+    [Table("Employees")]
+    public class Employee
+    {
+        [Key]
+        public int EmployeeId { get; set; }
+
+        [Required]
+        [StringLength(100)]
+        public string FirstName { get; set; } = string.Empty;
+
+        [Required]
+        [StringLength(100)]
+        public string LastName { get; set; } = string.Empty;
+
+        [NotMapped]
+        public string FullName => $"{FirstName} {LastName}";
+
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; } = string.Empty;
+
+        [Required]
+        public string JobTitle { get; set; } = string.Empty;
+
+        public bool IsActive { get; set; }
+
+        [Required]
+        public int DepartmentId { get; set; }
+
+        [ForeignKey(nameof(DepartmentId))]
+        public Department Department { get; set; } = null!;
+    }
+}
