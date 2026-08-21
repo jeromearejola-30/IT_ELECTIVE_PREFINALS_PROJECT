c2e23d90ba5966bb3e403cfdffb6414cbded59ac
jpacaonces <janssen.m.pacaonces@gmail.com>
2026-08-21 23:45:53 +0800

    added customer controller

---
+++ PREFINALS_PROJECT/PREFINALS_PROJECT/Controllers/CustomersController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace PREFINALS_PROJECT.Controllers
+{
+    public class CustomersController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
