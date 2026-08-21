f6d184607ff158c4dad9377d4f8c09b1c9098a38
jpacaonces <janssen.m.pacaonces@gmail.com>
2026-08-21 23:44:42 +0800

    added teams controller

---
+++ PREFINALS_PROJECT/PREFINALS_PROJECT/Controllers/TeamsController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace PREFINALS_PROJECT.Controllers
+{
+    public class TeamsController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
